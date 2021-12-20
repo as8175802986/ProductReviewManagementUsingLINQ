8dd97a2429374fb1d591325b65e53822ce3d344e
“Arunesh <as8175802986@gmail.com>
2021-12-20 23:48:37 +0530

    [Arunesh]Added Code for Product Review

--- ProductReviewMgmt/Program.cs
+++ ProductReviewMgmt/Program.cs
@@ -19,7 +19,17 @@ namespace ProductReviewMgmt
                 new ProductReview(){ProductId=7,UserId=7,Rating=4,Review="Average",isLike=true},
                 new ProductReview(){ProductId=8,UserId=8,Rating=5,Review="Excellent",isLike=true},
                 new ProductReview(){ProductId=9,UserId=9,Rating=3,Review="Good",isLike=true},
-                new ProductReview(){ProductId=10,UserId=10,Rating=5,Review="Excellent",isLike=true}
+                new ProductReview(){ProductId=10,UserId=10,Rating=5,Review="Excellent",isLike=true},
+                new ProductReview(){ProductId=11,UserId=11,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=12,UserId=12,Rating=4,Review="Average",isLike=true},
+                new ProductReview(){ProductId=13,UserId=13,Rating=5,Review="Excellent",isLike=true},
+                new ProductReview(){ProductId=14,UserId=14,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=15,UserId=15,Rating=2,Review="Poor",isLike=true},
+                new ProductReview(){ProductId=16,UserId=16,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=17,UserId=17,Rating=4,Review="Average",isLike=true},
+                new ProductReview(){ProductId=18,UserId=18,Rating=2,Review="Poor",isLike=true},
+                new ProductReview(){ProductId=19,UserId=19,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=20,UserId=20,Rating=5,Review="Excellent",isLike=true}
             };
             Management management = new Management();
             //UC1
