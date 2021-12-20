181c296e18c11487a1b84bb92a81ce1e137f3eab
“Arunesh <as8175802986@gmail.com>
2021-12-20 23:39:17 +0530

    [Arunesh]Added Code for Product Review Mgmt Using LINQ

---
+++ ProductReviewMgmt/Management.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewMgmt
+{
+    class Management
+    {
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from ProductReview in listProductReview orderby ProductReview.Rating descending select ProductReview).Take(5);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId" + list.ProductId +"-"+ "UserId" + list.UserId + "-" + "Rating" + list.Rating + "-" + "Review" + list.Review + "-"+ "isLike" + list.isLike);
+            }
+        }
+
+        
+    }
+}
---
+++ ProductReviewMgmt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewMgmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management using LINQ\n");
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductId=1,UserId=1,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=2,UserId=2,Rating=4,Review="Average",isLike=true},
+                new ProductReview(){ProductId=3,UserId=3,Rating=5,Review="Excellent",isLike=true},
+                new ProductReview(){ProductId=4,UserId=4,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=5,UserId=5,Rating=5,Review="Excellent",isLike=true},
+                new ProductReview(){ProductId=6,UserId=6,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=7,UserId=7,Rating=4,Review="Average",isLike=true},
+                new ProductReview(){ProductId=8,UserId=8,Rating=5,Review="Excellent",isLike=true},
+                new ProductReview(){ProductId=9,UserId=9,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProductId=10,UserId=10,Rating=5,Review="Excellent",isLike=true}
+            };
+            Management management = new Management();
+            //UC1
+            management.TopRecords(productReviewList);
+           
+            
+
+
+
+        }
+    }
+}
